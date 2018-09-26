21cfee40f6f3c5e0d42abf21ce1650afcf861f77
galih977 <galih977@gmail.com>
2018-09-26 13:52:14 +0700

    Create galih-disqus-notif.js

---
+++ galih-disqus-notif.js
@@ -0,0 +1,81 @@
+
+
+document.write(' \
+<style type="text/css" media="screen">\
+	 .dsq-widget ul.dsq-widget-list {\
+	 padding: 0;\
+	 margin: 0;\
+	 text-align: left;\
+	 }\
+	 img.dsq-widget-avatar {\
+	 width: 32px;\
+	 height: 32px;\
+	 border: 0px;\
+	 margin: 0px;\
+	 padding: 0px 3px 3px 0px;\
+	 float: left;\
+	 }\
+	 a.dsq-widget-user {\
+	 font-weight: bold;\
+	 }\
+	 a.dsq-widget-thread {\
+	 font-weight: bold;\
+	 }\
+	 p.dsq-widget-meta {\
+	 clear: both;\
+	 font-size: 80%;\
+	 padding: 0;\
+	 margin: 0;\
+	 }\
+	 li.dsq-widget-item {\
+	 margin: 15px 0;\
+	 list-style-type: none;\
+	 clear: both;\
+	 }\
+	 span.dsq-widget-clout {\
+	 padding: 0 2px;\
+	 background-color: #ff7300;\
+	 color: #fff;\
+	 }\
+	 table.dsq-widget-horiz td {\
+	 padding-right: 15px;\
+	 }\
+	 .dsq-widget-comment p {\
+	 display: inline;\
+	 }\
+	 </style>\
+	 <ul class="dsq-widget-list">\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/disqus_SJfRG5jiFe/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/21157/5768/avatar92.jpg?1465911301"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/disqus_SJfRG5jiFe/">Sharo-chan</a>\
+	 <span class="dsq-widget-comment"><p>Maaf mba klo ukuran gambar bisa diubah tidak? Klo bisa bgmn crnya?</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://www.arlinadzgn.com/2015/06/recent-post-widget-dengan-navigasi.html">\
+	 Recent Post Widget dengan Navigasi\
+	 </a>&nbsp;&middot;&nbsp;<a href="http://www.arlinadzgn.com/2015/06/recent-post-widget-dengan-navigasi.html#comment-4113259170">17 hours ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/mazdesu/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/15931/9857/avatar92.jpg?1476596341"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/mazdesu/">Akhmad Syahroni</a>\
+	 <span class="dsq-widget-comment"><p>Mba arlina sebenarnya yang mana yang lebih penting, artikel yang singkat dan padat tanpa basa basi, atau artikel yang talktative dan panjang untuk update Google Core?</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://www.arlinadzgn.com/2018/01/mengatasi-penurunan-trafik-update-google-core.html">Mengatasi Penurunan Trafik Blog Akibat Algoritma Google Core - Arlina Design</a>&nbsp;&middot;&nbsp;<a href="https://www.arlinadzgn.com/2018/01/mengatasi-penurunan-trafik-update-google-core.html#comment-4112699205">1 day ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/arifindahlan/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/27759/9434/avatar92.jpg?1537818844"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/arifindahlan/">Arifin Dahlan</a>\
+	 <span class="dsq-widget-comment"><p>di template infinite jga begitu. langkah langkahnya udah saya ikuti dengn baik tapi gak work</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://www.arlinadzgn.com/2018/07/pembaruan.komentar.disqus.dengan.onclick.event.html">Pembaruan Komentar Disqus dengan Onclick Event - Arlina Design</a>&nbsp;&middot;&nbsp;<a href="https://www.arlinadzgn.com/2018/07/pembaruan.komentar.disqus.dengan.onclick.event.html#comment-4112152211">1 day ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/geryanggriawan/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/16206/410/avatar92.jpg?1537762425"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/geryanggriawan/">Gery Anggriawan</a>\
+	 <span class="dsq-widget-comment"><p>Keren euy. Makasih. :D</p></span>\
+	 <p class="dsq-widget-meta"><a href="https://www.arlinadzgn.com/2018/07/mengganti.permalink.menjadi.seperti.wordpress.html">Mengganti Format Permalink Menjadi Seperti WordPress - Arlina Design</a>&nbsp;&middot;&nbsp;<a href="https://www.arlinadzgn.com/2018/07/mengganti.permalink.menjadi.seperti.wordpress.html#comment-4110976623">2 days ago</a></p>\
+	 </li>\
+	 <li class="dsq-widget-item">\
+	 <a href="https://disqus.com/by/fauzanid/"><img class="dsq-widget-avatar" src="https://c.disquscdn.com/uploads/users/12360/9116/avatar92.jpg?1537632612"></a>\
+	 <a class="dsq-widget-user" href="https://disqus.com/by/fauzanid/">Ahmad Fauzan</a>\
+	 <span class="dsq-widget-comment"><p>ditunggu :D</p></span>\
+	 <p class="dsq-widget-meta"><a href="http://www.arlinadzgn.com/2014/08/off-topic.html">Off Topic - Arlina Design</a>&nbsp;&middot;&nbsp;<a href="http://www.arlinadzgn.com/2014/08/off-topic.html#comment-4108757564">3 days ago</a></p>\
+	 </li>\
+	 </ul>\
+');
